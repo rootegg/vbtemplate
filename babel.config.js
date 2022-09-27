d9572bc0953caaf1391efac49ac7a5ebc9e24b94
rootegg <344163452@qq.com>
2022-09-27 20:56:30 +0800

    init

---
+++ babel.config.js
@@ -0,0 +1,48 @@
+module.exports = {
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        useBuiltIns: "usage",
+        corejs: 2,
+        modules: false,
+      },
+    ],
+  ],
+  plugins: [
+    ["@babel/plugin-transform-runtime"],
+    [
+      "babel-plugin-import",
+      {
+        libraryName: "vant",
+        libraryDirectory: "es",
+        style: true,
+      },
+      "vant",
+    ],
+    [
+      "babel-plugin-import",
+      {
+        libraryName: "antd",
+        style: true, // or 'css'
+      },
+    ],
+    [
+      "babel-plugin-import",
+      {
+        "libraryName": "lodash",
+        "libraryDirectory": "",
+        "camel2DashComponentName": false,  // default: true
+      },
+      "lodash",
+    ],
+    [
+      "babel-plugin-component",
+      {
+        libraryName: "element-ui",
+        styleLibraryName: "theme-chalk",
+      },
+      "element-ui",
+    ],
+  ],
+};
---
+++ webpack.config.js
@@ -0,0 +1,146 @@
+const path = require("path");
+const webpack = require("webpack");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const TerserWebpackPlugin = require("terser-webpack-plugin"); // webpack5 包中自动包含terser-webpack-plugin包，不需要单独安装
+const CssMinimizerWebpackPlugin = require("css-minimizer-webpack-plugin");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const { VueLoaderPlugin } = require("vue-loader");
+const { merge } = require("webpack-merge");
+const isDevelopment = process.env.env == "development";
+const isProduction = process.env.env == "production";
+const isAnalyzer = process.env.env == "analyzer";
+
+/**
+ * @type {webpack.Configuration} // 自动提示
+ */
+const webpackConfig = {
+  entry: {
+    index: "./src/index.js",
+  },
+  output: {
+    clean: true, // 清空dist目录，可以代替CleanWebpackPlugin
+    path: path.resolve(__dirname, "dist"), // 输入目录，绝对路径，默认dist
+    filename: "[name].[chunkhash].js", // js用chunkhash
+    chunkFilename: "[name].[chunkhash].js", // 通常是一个入口一个chunk，但是异步请求import('...')或require.ensure('...')会形成chunk，optimization.splitChunk可以独立chunk
+    assetModuleFilename: "assets/[name].[contenthash].[ext][query]", // 图片等素材文件夹和命名用contenthash
+  },
+  mode: "production",
+  module: {
+    rules: [
+      // {
+      //   test: /\.js$/,
+      //   exclude: /node_modules/,
+      //   use: ["babel-loader"],
+      // },
+      {
+        test: /\.vue$/,
+        use: ["vue-loader"],
+      },
+      {
+        test: /(\.css|\.scss)$/,
+        use: [
+          isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /(\.jpg|\.jpeg|\.png|\.gif|\.webp)$/,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf)$/,
+        type: "asset/resource",
+      },
+    ],
+  },
+  resolve: {
+    alias: {
+      "@/": path.resolve("./src"),
+    },
+    extensions: [".js", ".jsx"],
+  },
+  devServer: {
+    open: true,
+    port: 3000,
+    hot: true,
+  },
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+      cacheGroups: {
+        elementUi: {
+          name: "element-ui",
+          test: /[\\/]node_modules[\\/]element-ui[\\/]/,
+          priority: 10,
+        },
+      },
+    },
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlWebpackPlugin({
+      title: "模板网页",
+      template: "./index.html",
+      chunks: ["index"],
+      minify: false, // 是否压缩html
+    }),
+    /**
+     * 提供给代码中可以使用变量，如果是字符串，要多包一层引号，否则会被当着变量
+     */
+    new webpack.DefinePlugin({
+      MODE: JSON.stringify(process.env.env),
+      BUILD: "'build'",
+    }),
+  ].filter(Boolean),
+};
+
+if (isDevelopment) {
+  /**
+   * 只开发环境要 source-map
+   */
+  merge(webpackConfig, {
+    mode: "development",
+    output: {
+      clean: false,
+    },
+    devtool: "source-map",
+  });
+} else {
+  merge(webpackConfig, {
+    plugins: [
+      /**
+       * 清空dist目录
+       */
+      // new CleanWebpackPlugin(),
+      /**
+       * 提取css到独立文件
+       */
+      new MiniCssExtractPlugin({
+        filename: "[name].[contenthash].css", //css和图片素材用contenthash
+        chunkFilename: "[id].css",
+      }),
+      /**
+       * 压缩js文件, 观察到production时js已经被压缩
+       */
+      // new TerserWebpackPlugin(),
+    ],
+    // optimization: {
+    //   minimize: true,
+    //   minimizer: [
+    //     new CssMinimizerWebpackPlugin(), //  观察到production时css已经被压缩
+    //   ],
+    // },
+  });
+  if (isAnalyzer) {
+    // 体积分析
+    merge(webpackConfig, {
+      plugins: [new BundleAnalyzerPlugin()],
+    });
+  }
+}
+
+module.exports = webpackConfig;
