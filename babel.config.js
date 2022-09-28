2b52a52fdcd6b168a1cbb520159caec64f41660e
rootegg <344163452@qq.com>
2022-09-28 19:55:00 +0800

    feat: react

--- babel.config.js
+++ babel.config.js
@@ -1,48 +1,55 @@
 module.exports = {
   presets: [
     [
-      "@babel/preset-env",
+      '@babel/preset-env',
       {
-        useBuiltIns: "usage",
+        useBuiltIns: 'usage',
         corejs: 2,
-        modules: false,
-      },
+        modules: false
+      }
     ],
+    [
+      '@babel/preset-react',
+      {
+        runtime: 'automatic' // classic automatic
+      }
+    ],
+    '@babel/preset-typescript'
   ],
   plugins: [
-    ["@babel/plugin-transform-runtime"],
+    ['@babel/plugin-transform-runtime'],
     [
-      "babel-plugin-import",
+      'babel-plugin-import',
       {
-        libraryName: "vant",
-        libraryDirectory: "es",
-        style: true,
+        libraryName: 'vant',
+        libraryDirectory: 'es',
+        style: true
       },
-      "vant",
+      'vant'
     ],
     [
-      "babel-plugin-import",
+      'babel-plugin-import',
       {
-        libraryName: "antd",
-        style: true, // or 'css'
-      },
+        libraryName: 'antd',
+        style: true // or 'css'
+      }
     ],
     [
-      "babel-plugin-import",
+      'babel-plugin-import',
       {
-        "libraryName": "lodash",
-        "libraryDirectory": "",
-        "camel2DashComponentName": false,  // default: true
+        libraryName: 'lodash',
+        libraryDirectory: '',
+        camel2DashComponentName: false // default: true
       },
-      "lodash",
+      'lodash'
     ],
     [
-      "babel-plugin-component",
+      'babel-plugin-component',
       {
-        libraryName: "element-ui",
-        styleLibraryName: "theme-chalk",
+        libraryName: 'element-ui',
+        styleLibraryName: 'theme-chalk'
       },
-      "element-ui",
-    ],
-  ],
-};
+      'element-ui'
+    ]
+  ]
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
-import Vue from 'vue'
-import App from './app.vue'
+// import Vue from 'vue'
+// import VueApp from './vue/app.vue'
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+import ReactApp from './react/app'
 import { f1 } from './util'
 console.log(f1)
-new Vue({
-  render: h => h(App)
-}).$mount('#root')
+/**
+ * vue
+ */
+// new Vue({
+//   render: h => h(VueApp)
+// }).$mount('#root')
+
+/**
+ * react
+ */
+const container = document.getElementById('app')
+const root = createRoot(container) // createRoot(container!) if you use TypeScript
+root.render(<ReactApp />)
