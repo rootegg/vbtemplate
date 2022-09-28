92288d21478988b2b7b73af04abf187f8154bf05
rootegg <344163452@qq.com>
2022-09-28 12:30:58 +0800

    feat: lint

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import Vue from "vue";
-import App from "./app.vue";
-import { f1 } from "./util";
+import Vue from 'vue'
+import App from './app.vue'
+import { f1 } from './util'
 console.log(f1)
 new Vue({
-  render: (h) => h(App),
-}).$mount("#root");
+  render: h => h(App)
+}).$mount('#root')
