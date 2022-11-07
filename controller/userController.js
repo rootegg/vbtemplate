708a6699f1b64f8aa1df9d7573cc3c1a561428a4
rootegg <344163452@qq.com>
2022-11-07 14:48:36 +0800

    curl koa

--- controller/userController.js
+++ controller/userController.js
@@ -13,24 +13,28 @@ const userRequestUrlMappingResolver = require("../config/client/userRequestUrlMa
     }
  }
  */
-
+// curl -X POST -H "Content-Type:application/json;charset=utf-8" -d '{"name":"hello","password":"123"}' http://localhost:3000/api/login
 class UserController {
   async login(ctx) {
     const requestUrl = userRequestUrlMappingResolver.login;
     console.log(ctx.request.body);
-    const response = await baseHttpClient.doHttpPostRequest(
-      ctx,
-      requestUrl,
-      JSON.stringify(ctx.request.body)
-    );
-    const responseData = qs.parse(response.data);
-    const responseDataCode = responseData.result.code;
+    try {
+      const response = await baseHttpClient.doHttpPostRequest(
+        ctx,
+        requestUrl,
+        JSON.stringify(ctx.request.body)
+      );
+      const responseData = qs.parse(response.data);
+      const responseDataCode = responseData.result.code;
 
-    // login successful
-    if (0 === responseDataCode) {
-      ctx.body = responseData;
-    } else {
-      ctx.body = responseData;
+      // login successful
+      if (0 === responseDataCode) {
+        ctx.body = responseData;
+      } else {
+        ctx.body = responseData;
+      }
+    } catch (e) {
+      ctx.body = "转发测试连接不通";
     }
   }
 }
